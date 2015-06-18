c0cc1fcb3f283715a4a06af59f61ab8044afe5f8
Duyet Nguyen <duyet.nguyen@outlook.com>
2015-06-18 16:31:14 +0700

    update redis.rb to run redis on heroku

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,8 @@
-$redis = Redis.new(:host => 'localhost', :port => 6379)
+# $redis = Redis.new(:host => 'uri.host', :port => uri.port, :password => uri.password)
+# if ENV["REDISCLOUD_URL"]
+  uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
+  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+# else
+#   $redis = Redis.new(:host => 'localhost', :port => 6379)
+#
+# end
